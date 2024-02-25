8e42bb9df8c1944ca170f8d4291c8f3d46f63a09
Gabriel <gabriellfsouza@gmail.com>
2024-02-25 20:36:26 +0000

    chore: first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# DDD (Domain-driven Design)
+
+Design dirigido à domínio
+
+- Domain Experts
+  - Conversa
+- Linguagem ubíqua
+
+- Usuário
+  - Cliente
+  - Fornecedor
+  - Atendente
+  - Barman
+
+- Agragados
+- Value Objects
+- Eventos de domínio
+- Subdomínios (Bounded Contexts)
+- Entidades
+- Casos de Uso
---
+++ src/core/entities/unique-entity-id.ts
@@ -0,0 +1,19 @@
+import { randomUUID } from "node:crypto"
+
+export class UniqueEntityId {
+  toString() {
+    return this.value
+  }
+
+  toValue(){
+    return this.value
+  }
+
+  constructor(private value: string = randomUUID()){}
+  // get id(){
+  //   return this.value
+  // }
+  // static create (id?: string){
+  //   return new UniqueEntityId(id ?? crypto.randomUUID())
+  // }
+}
